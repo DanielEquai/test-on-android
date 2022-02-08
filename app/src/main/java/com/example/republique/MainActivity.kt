746e2be215a4d9df1eb7206db4a7152cd0929bf7
DanielEquai <equaidaniel18@gmail.com>
2022-02-08 19:42:26 +0800

    i don try tire

---
+++ app/src/main/java/com/example/republique/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.republique
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT: Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler()
+            .postDelayed(
+                {
+                    startActivity(Intent(this, SplashScreen::class.java))
+                    finish()
+                }, SPLASH_TIME_OUT
+            )
+    }
+}
